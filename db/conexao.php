f0de58a80e36e20f8a2e3df08f1cbaf00a0862b7
Code <Code@10.30.32.37>
2015-10-29 18:46:12 -0200

    Criação da classe de conexão

---
+++ db/conexao.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * @author: Daniel Fiuza
+ * @date: 29/10/2015 18:43
+ *  
+ */
+class Conexao{
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $banco = 'automecanica';
+    
+    private $conexao;
+    private $resultado;
+
+
+    function conectar(){
+        $this->conexao = mysqli_connect($this->host ,$this->user,$this->password);
+        mysqli_select_db($this->conexao,$this->banco) or die(trigger_error('Erro ao selecionar banco de dados'));
+    }
+    
+    
+    public function desconectar(){
+        mysqli_close($this->conexao);
+    }
+    
+    //essa funcção só funciona para insert,delete e update
+    public function executar($sql){
+        $this->conectar();
+        $this->resultado = mysqli_query($this->conexao, $sql);
+          $this->desconectar();
+    }
+    
+    public function recuperarValores($sql){
+        $this->conectar();
+        $this->resultado = mysqli_query($this->conexao, $sql);
+        
+        $registros = array();
+       while($dados = mysqli_fetch_assoc($this->resultado)) {
+          $registros[] = $dados; 
+       }
+       $this->desconectar();
+       return $registros;
+    }
+   // 
+  // 
+
+}
